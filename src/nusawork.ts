76cd4f2e6a30cdd9cd64277dafbf5ea7a4de0e6e
Wardi <wardi@nusa.net.id>
2024-09-07 07:00:21 +0700

    Update: Include additional departments in fetchNusaworkPresentEngineers API request

--- src/nusawork.ts
+++ src/nusawork.ts
@@ -71,7 +71,7 @@ export async function fetchNusaworkPresentEngineers(
     params: {
       status: 'working,clock_out',
       id_branch: '5',
-      id_department: '31',
+      id_department: '31,49,50,51',
       sort_by: 'name',
       order_by: 'asc',
     },
